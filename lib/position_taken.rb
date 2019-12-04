366ce1ea06f0a16ccb7442bbef0ff0625efeba45
Erin Stansbury <ekstansbury@gmail.com>
2019-12-04 15:54:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if array[index_number] == " " || array[index_number] == "" || array[index_number] == nil
     puts false
-  else
+  else array[index_number] == "X" || array[index_number] == "O"
     puts true
   end
 end
