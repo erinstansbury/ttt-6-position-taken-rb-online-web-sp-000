41fb1fc7bfcbd9a8a08c1496f06d17e7122ba4f5
Erin Stansbury <ekstansbury@gmail.com>
2019-12-04 15:57:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if array[index_number] == " " || array[index_number] == "" || array[index_number] == nil
+  if board[index_number] == " " || array[index_number] == "" || array[index_number] == nil
     return false
   else array[index_number] == "X" || array[index_number] == "O"
     return true
