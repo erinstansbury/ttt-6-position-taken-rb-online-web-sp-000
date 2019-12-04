6ecd4b7ee11f6dcf646d87f420b566b680c14f7a
Erin Stansbury <ekstansbury@gmail.com>
2019-12-04 15:45:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,1 @@
 # code your #position_taken? method here!
-def position_taken(board, index)
-  if array[index_number] == " " || array[index_number] == "" || array[index_number] == nil
-    puts false
-  else 
-    puts true
-end
